d1fda9a3e706ce4a6df4c84179f1035d765b0b42
Sarah Davis <170495238+SarahDavis2@users.noreply.github.com>
2025-07-24 21:43:49 -0500

    removeFrom Array() with .filter()

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...removeArgs) {
+    return arr.filter( (item) => {
+        let addToFlag = true;
+        for (let arg of removeArgs) {
+            if (item === arg) {
+                addToFlag = false;
+                break;
+            }    
+        }
+
+        return addToFlag;
+    });
 };
 
 // Do not edit below this line
