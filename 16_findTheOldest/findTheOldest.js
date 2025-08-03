7fa643783a6f7fa68a7b98c31a5c3c51860372a9
Sarah Davis <170495238+SarahDavis2@users.noreply.github.com>
2025-08-03 16:40:20 -0500

    first solution

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.sort((itemObjA, itemObjB) => {
+        return calcAge(itemObjB) - calcAge(itemObjA);
+    });
 
-};
+    return people.at(0);
+}
 
+function calcAge(obj) {
+    let age = 0;
+    if ("yearOfDeath" in obj) {
+        age = obj.yearOfDeath - obj.yearOfBirth;
+    } else {
+        const curYr = new Date().getFullYear();
+        age = curYr - obj.yearOfBirth;
+    }
+    return age;
+}
 // Do not edit below this line
 module.exports = findTheOldest;
