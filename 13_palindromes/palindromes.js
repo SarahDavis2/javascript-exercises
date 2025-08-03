657eeb7c49ff7ca41b82cbc56bb7b19f64734eed
Sarah Davis <170495238+SarahDavis2@users.noreply.github.com>
2025-08-03 14:34:26 -0500

    og palindrome solution

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let isPalindrome = true;
 
+    str = str.toLowerCase();
+    // Remove symbols and punctuation and spaces
+    str = str.replace(/[^a-z0-9]/g, ''); 
+    
+    for (let count = 0; count < str.length; count++) {
+        if (str.at(count) != str.at(-count - 1)) {
+            isPalindrome = false;
+        }
+    }
+
+    return isPalindrome;
 };
 
 // Do not edit below this line
