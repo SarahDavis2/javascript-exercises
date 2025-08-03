55486d7b6cbbe536f015b43a882c0d978a1b3b78
Sarah Davis <170495238+SarahDavis2@users.noreply.github.com>
2025-08-03 15:16:30 -0500

    fibonacci seq

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (typeof num !== 'number') num = parseInt(num);
+    if (num < 0) return "OOPS";
+    if (num == 0) return 0;
 
+    const arr = [0, 1];
+    for (let i = 2; i <= num; i++) {
+        arr[i] = arr[i - 1] + arr[i - 2];
+    }
+    return arr[num];
 };
 
 // Do not edit below this line
