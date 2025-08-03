10e89154cb11895cf6b13ee21631a1e4de68322c
Sarah Davis <170495238+SarahDavis2@users.noreply.github.com>
2025-08-03 16:45:23 -0500

    cleanup code

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -2,14 +2,12 @@ const findTheOldest = function(people) {
     people.sort((personA, personB) => {
         return calcAge(personB) - calcAge(personA);
     });
-
     return people.at(0);
 }
 
 function calcAge(person) {
-    // Nullish coalescing assignment operator if yearOfDeath is empty
-    person.yearOfDeath ??= new Date().getFullYear();
-
+    // Nullish coalescing assignment operator if yearOfDeath is empty set to current year
+    person.yearOfDeath ??= new Date().getFullYear(); 
     return person.yearOfDeath - person.yearOfBirth;
 }
 // Do not edit below this line
